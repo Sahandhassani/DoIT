6adde0ad1b458ed1f79f2a202852830b18840128
Sahand Hassani <sahandhassani@gmail.com>
2022-08-11 21:23:04 +0430

    persons added

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("HELLO WORLD")
+var persons = {id1:{
+    name: "sahand",
+    age: 27,
+    educations: ['bachelor','master']
+},
+id2:{
+    name: "hamid",
+    age: 35,
+},
+id3:{
+    name: "reza",
+    age: 30,
+},
+};
+
+console.log(persons
+    );
